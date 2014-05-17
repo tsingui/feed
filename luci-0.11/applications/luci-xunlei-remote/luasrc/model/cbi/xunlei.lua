78b6f6d212299b4351743b6d77654f9721042d58
jackson <jackson@74e81aba-36f0-4891-8cfb-1815f9025a0c>
2014-05-17 12:33:10 +0000

    Try to fix the missing section of luci-app-xunlei

--- luci-0.11/applications/luci-xunlei-remote/luasrc/model/cbi/xunlei.lua
+++ luci-0.11/applications/luci-xunlei-remote/luasrc/model/cbi/xunlei.lua
@@ -1,3 +1,17 @@
+--[[
+LuCI - Lua Configuration Interface - Xunlei Xware support
+
+Copyright 2012 Hasc
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+$Id$
+]]--
+
 local fs = require "nixio.fs"
 local util = require "nixio.util"
 
